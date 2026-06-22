1eda7dd26ab3ae54d37f7429574df65243a066d3
gaviriamaicol2008-creator <gaviriamaicol2008@gmail.com>
2026-06-22 11:21:04 -0500

    se arreglo el codigo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 //Capturamos los elementos usando el DOM
 
-const mensajeError = document.getElementById("MensajeError");
+const mensajeError = document.getElementById("mensajeError");
 const btnAgregar = document.getElementById("btnAgregar");
-const imputActividad = document.getElementById("actividadImput")
-const listaActividades = document.getElementById("listaActividades")
-const mensajeVacio =document.getElementById("mensajeVacio")
-
-const textoActividad =document.getElementById("totalActividades")
-const totalActividadRealizada =document.getElementById("actividadesRealizadas")
-const actividadesPendientes =document.getElementById("actividadesPendientes")
+const inputActividad = document.getElementById("actividadInput");
+const listaActividades = document.getElementById("listaActividades");
+const mensajeVacio =document.getElementById("mensajeVacio");
+document.getElementById("year").textContent =new Date().getFullYear();
+const totalActividades =document.getElementById("totalActividades");
+const totalActividadesRealizadas =document.getElementById("totalActividadesRealizadas");
+const totalActividadesPendientes =document.getElementById("totalActividadesPendientes");
 
 // Evento Para Agregar Actividad Al Hacer Click En El Boton 
 btnAgregar.addEventListener("click", agregarActividad);
 
 inputActividad.addEventListener("keypress", function(evento){
     if(event.key ===""){
-        mensajeError.textContent
+        agregarActividad();
     }
-}
+});
 
 
 
 // Funcion principal para agregar una actividad 
 function agregarActividad() {
-    const textoActividad = imputActividad.Value.trim();
+    const textoActividad = inputActividad.value.trim();
 
     // Validacion de campo vacio
     if( textoActividad === "") {
@@ -51,37 +51,45 @@ function agregarActividad() {
 
     // Botón para marcar como realizada
     const btnRealizada = document.createElement("button");
-    btnRealizada.textContent = "Realizado";
+    btnRealizada.textContent = "Realizada";
     btnRealizada.classList.add("btn-realizada");
 
     // Botón para marcar como eliminada
 
-    const btnEliminada = document.createElement("Eliminatebutton");
-    btnEliminada.textContent = "Eliminar";
-    btnEliminada.classList.add("btn-eliminar");
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "Eliminar";
+    btnEliminar.classList.add("btn-eliminar");
     
 
     // Evento para marcar o desenmarcar como realizado
 
-    btnRealizada.addEventListener("click", function(){
+    btnRealizada.addEventListener("click", function () {
         nuevaActividad.classList.toggle("realizada");
 
-        if(nuevaActividad.classList.contains("realizada")){
-            btnRealizada.textContent = "Realizada";
-        } else {
+        if(nuevaActividad.classList.contains("realizada")) {
             btnRealizada.textContent = "Pendiente";
+        } else {
+            btnRealizada.textContent = "Realizada";
         }
     });
 
-    btnEliminada.addEventListener("click", function(){
+    btnEliminar.addEventListener("click", function(){
         listaActividades.removeChild(nuevaActividad);
     actualizarContadores();
     });
+
     //Agregamos los botones al contador 
     contenedorBotones.appendChild(btnRealizada);
     contenedorBotones.appendChild(btnEliminar);
     
+    //Agregamos el texto a los botones al li
+
+    nuevaActividad.appendChild(texto);
+    nuevaActividad.appendChild(contenedorBotones);
+
     listaActividades.appendChild(nuevaActividad);
+
+    actualizarContadores();
 
 }
 
@@ -91,6 +99,19 @@ function actualizarContadores() {
     const realizadas = listaActividades.querySelectorAll(".realizada");
 
     const total = actividades.length;
-    const totaRealizadas = realizadas.length;
-    const totalPendientes = total-totaRealizadas; 
- }
+    const totalRealizadas = realizadas.length;
+    const totalPendientes = total - totalRealizadas; 
+    
+   totalActividades.textContent = total;
+   totalActividadesRealizadas.textContent = totalRealizadas;
+   totalActividadesPendientes.textContent = totalPendientes;
+
+   //Mostrar u ocultar mensaje cuando la lista este vacía
+
+   if(total === 0){
+      mensajeVacio.style.display = "block";
+   } else {
+      mensajeVacio.style.display = "none";
+   }
+
+}
